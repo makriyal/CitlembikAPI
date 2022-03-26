c57a759bd1334c9c8dd93f398b4e374671d1c0d2
makriyal <33030337+makriyal@users.noreply.github.com>
2022-03-27 02:17:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,30 +25,72 @@ class Scraper():
                 }
                 results_list.append(item)
         elif site == "halk":
-            if "index.php?" not in sorgu:
+            if sorgu.isdigit():
+                r = s.get("https://www.halkkitabevi.com/index.php?p=Products&q_field_active=0&ctg_id=&q="+sorgu)
+                try:
+                    title = r.html.find('div > div.col2.__col2 > h1', first=True).text.strip()
+                except:
+                    title = ''
+                try:
+                    img = r.html.find('#main_img', first=True).attrs['src']
+                except:
+                    img = ''
+                try:
+                    img_alt = r.html.find('#main_img', first=True).attrs['data-zoom-image']
+                except:
+                    img_alt = ''
+                try:
+                    publisher = r.html.find('div.prd_brand_box > a.publisher', first=True).text.strip()
+                except:
+                    publisher = ''
+                try:
+                    author = r.html.find('div.prd_info > div.writer > a', first=True).text.strip()
+                except:
+                    author = ''
+                item = {
+                        'title' : title,
+                        'img' : img,
+                        'img_alt' : img_alt,
+                        'link' : sorgu,
+                        'publisher' : publisher,
+                        'author' : author,
+                    }
+                results_list.append(item)
+            else :
                 r = s.get("https://www.halkkitabevi.com/index.php?p=Products&" + sorgu)
                 results = r.html.find('div.prd_list_container_box > div > ul > li > div')
                 for res in results :
+                    try:
+                        title = res.find('a>img', first=True).attrs['title']
+                    except:
+                        title = ''
+                    try:
+                        img = res.find('a>img', first=True).attrs['data-src']
+                        img_alt = res.find('a>img', first=True).attrs['data-src']
+                    except:
+                        img = ''
+                        img_alt = ''
+                    try:
+                        link = res.find('a', first=True).attrs['href']
+                    except:
+                        link = ''
+                    try:
+                        publisher = res.find('div.prd_info > div.publisher', first=True).text.strip()
+                    except:
+                        publisher = ''
+                    try:
+                        author = res.find('div.prd_info > div.writer > a', first=True).text.strip()
+                    except:
+                        author = ''
                     item = {
-                        'title' : res.find('a>img', first=True).attrs['title'],
-                        'img' : res.find('a>img', first=True).attrs['data-src'],
-                        'img_alt' : res.find('a>img', first=True).attrs['data-src'],
-                        'link' : res.find('a', first=True).attrs['href'],
-                        'publisher' : res.find('div.prd_info > div.publisher', first=True).text.strip(),
-                        'author' : res.find('div.prd_info > div.writer > a', first=True).text.strip(),
+                        'title' : title,
+                        'img' : img,
+                        'img_alt' : img_alt,
+                        'link' : link,
+                        'publisher' : publisher,
+                        'author' : author,
                     }
                     results_list.append(item)
-            else :
-                r = s.get(sorgu)
-                item = {
-                        'title' : r.html.find('div > div.col2.__col2 > h1', first=True).text.strip(),
-                        'img' : r.html.find('#main_img', first=True).attrs['src'],
-                        'img_alt' : r.html.find('#main_img', first=True).attrs['data-zoom-image'],
-                        'link' : sorgu,
-                        'publisher' : r.html.find('div.prd_brand_box > a.publisher', first=True).text.strip(),
-                        'author' : r.html.find('div.prd_info > div.writer > a', first=True).text.strip(),
-                    }
-                results_list.append(item)
                 
         return results_list
 
